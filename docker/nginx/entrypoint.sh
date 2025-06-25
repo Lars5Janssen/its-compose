b9430ee8dcd512c9a4003ca16880539681a7df93
Lars Janssen <lars.e.janssen@protonmail.com>
2025-06-25 20:57:15 +0200

    su

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -2,5 +2,5 @@
 seconds_to_live=$2
 nginx
 tcpdump -w "/tcpdumpinfo/tcpdump.$(date).$1.pcap" -G $seconds_to_live -W 1
-# while true; 
-# do sleep 1; sleep 1; done
+while true; 
+do sleep 1; sleep 1; done
