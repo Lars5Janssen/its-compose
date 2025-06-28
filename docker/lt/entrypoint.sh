174d08a30755c669ba35f08f3efaeaa5e6852685
Lars Janssen <lars.e.janssen@protonmail.com>
2025-06-28 18:40:39 +0200

    lt final?

--- docker/lt/entrypoint.sh
+++ docker/lt/entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+echo "Starting Location Test Request to public url"
 sleep 3s
-curl crisp-kangaroo-modern.ngrok-free.app/app/LocationTest > /locationTest/uuid
+rm /locationTest/uuid
+curl --location crisp-kangaroo-modern.ngrok-free.app/app/LocationTest > /locationTest/uuid
+inhalt="$(cat /locationTest/uuid)"
+echo "Global uuid is: $inhalt"
